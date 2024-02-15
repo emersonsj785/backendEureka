bf82422bcd887fd4649aaac1993fed701e0637dc
emerson <jonatanb245sj@gmail.com>
2024-02-15 15:56:06 -0500

    feat: Servidor Eureka

---
+++ src/test/java/com/reto02/microservicios/app/eureka/microservicioseureka/MicroserviciosEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto02.microservicios.app.eureka.microservicioseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
